4d1b013dff918d4177d80581d8bd9dba5c8106f6
Gianni-Lauritz Grubert <animalibera@mail.de>
2024-10-13 07:39:35 +0200

    First Attemp Output to PDF

--- Script/create.py
+++ Script/create.py
@@ -1,15 +1,17 @@
 import yaml
+import sys
 import os
+from fpdf import FPDF
 
 def main():
 	content = load_yaml_file("../", "license.yaml")
-	print(recursive_print(content))
+	create_pdf(recursive_text(content).encode('latin-1', errors='replace').decode('latin-1'), "License.pdf")
 
 def load_yaml_file(path, name):
 	with open(path + name, "r") as file:
 		return yaml.safe_load(file)
 
-def recursive_print(content, text = "", indention = 0, stepsize = 2, paragraph = None):
+def recursive_text(content, text = "", indention = 0, stepsize = 2, paragraph = None):
 	headings = " " * indention
 	trailing = "" if paragraph is None else f" §{paragraph}"
 	term = content["Term"]
@@ -28,12 +30,19 @@ def recursive_print(content, text = "", indention = 0, stepsize = 2, paragraph =
 		text = text + os.linesep #Newline after Title before List of Elements
 		for index, element in enumerate(term):
 			alterd_paragraph = str(index + 1) if paragraph is None else paragraph + "." + str(index + 1)
-			text = recursive_print(element, text, indention + stepsize, stepsize, alterd_paragraph)
+			text = recursive_text(element, text, indention + stepsize, stepsize, alterd_paragraph)
 	#Error Case
 	else:
 		raise TypeError(f"The Type of {term} is not String or List")
 	
 	return text
+
+def create_pdf(content, file):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.cell(200, 10, txt=content, ln=1, align="C")
+    pdf.output(file)
 
 if __name__ == "__main__":
 	main()
